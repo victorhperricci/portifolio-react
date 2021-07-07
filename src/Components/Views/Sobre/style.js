5784f0ce8fe76071d1126b15439c586ed7e9683b
VictorHugo1603 <vitor.hugo@newpost.com>
2021-07-07 19:11:38 -0300

    acabando contato

--- src/Components/Views/Sobre/style.js
+++ src/Components/Views/Sobre/style.js
@@ -20,7 +20,7 @@ export const SobreContainer = styled(motion.section)`
   }
   .container-texts {
     width: 100%;
-    height: 100%;
+    height: auto;
     position: relative;
     display: flex;
     flex-direction: column;
